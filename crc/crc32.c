4f8f66d5e959fce2d9d1b62899681155eb179b72
Wang Yaofu <voipman@sina.cn>
2021-08-25 18:47:07 +0800

    add LSB-first comment and Poly value

--- crc/crc32.c
+++ crc/crc32.c
@@ -1,8 +1,14 @@
 /*
 ** The crc32 is licensed under the Apache License, Version 2.0, and a copy of the license is included in this file.
 **
-**Author:Wang Yaofu voipman@qq.com
-**Description: The source file of class crc32.
+** Author:Wang Yaofu voipman@qq.com
+** Description: The source file of class crc32.
+**  CRC32 implementation according to IEEE standards.
+**  Polynomials are represented in LSB-first form
+**  following parameters:
+**    Width                      : 32 bit
+**    Poly                       : 0xedb88320 (That is actually x^8 + x^5 + x^4 + 1)
+**    Output for "123456789"     : 0xCBF43926
 */
 #include <stdlib.h>
 #include "crc32.h"
